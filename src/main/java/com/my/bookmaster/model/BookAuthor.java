cf100096c1e126c15c109ca1767e741b044c84d4
Rodion Tkachenko <tkachenko_rp@mail.ru>
2025-06-23 00:54:57 +0700

    add models

---
+++ src/main/java/com/my/bookmaster/model/BookAuthor.java
@@ -0,0 +1,15 @@
+package com.my.bookmaster.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class BookAuthor {
+    private Long id;
+    private String name;
+}
